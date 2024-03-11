361880e8e2c70819d3377b0646120d73c804d809
cyruu <cyruz.mhr09@gmail.com>
2024-03-11 12:43:25 +0545

    first

---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import {
+  cellEmpty,
+  placeMark,
+  checkGameOver,
+  resetCells,
+} from "../features/methods";
+function TicTacToe() {
+  let crossMark = '<i class="ri-close-large-line"></i>';
+  let circleMark = '<i class="ri-circle-line"></i>';
+  const [xscore, setXScore] = useState(0);
+  const [oscore, setOScore] = useState(0);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [playerIcon, setPlayerIcon] = useState(crossMark);
+  const [gameOver, setGameOver] = useState(false);
+  const [winner, setWinner] = useState(null);
+  const [tie, setTie] = useState(false);
+  const [markCount, setMarkCount] = useState(0);
+  function handlePlayAgain() {
+    resetCells();
+    setGameOver(false);
+    setTie(false);
+    setMarkCount(0);
+    setGameOver(false);
+  }
+  useEffect(() => {
+    currentPlayer == "X" ? setPlayerIcon(crossMark) : setPlayerIcon(circleMark);
+  }, [currentPlayer]);
+  function handleClick({ target }) {
+    let cellId = target.id;
+    // if this cell is empty
+    if (cellEmpty(cellId)) {
+      // place mark
+      placeMark(cellId, playerIcon, setMarkCount);
+      // check game over
+      checkGameOver(
+        setGameOver,
+        setWinner,
+        crossMark,
+        circleMark,
+        setTie,
+        markCount,
+        winner,
+        setXScore,
+        setOScore
+      );
+
+      setCurrentPlayer((prev) => (prev == "X" ? "O" : "X"));
+    }
+  }
+
+  return (
+    <div className="main">
+      <h1 className="player">{currentPlayer}'s turn</h1>
+      <div className="scorecard">
+        <p>
+          X's Score: <span className="score">{xscore}</span>
+        </p>
+        <p>
+          O's Score: <span className="score">{oscore}</span>
+        </p>
+      </div>
+      <div id="slashmark"></div>
+      <div className="tictac" onClick={handleClick}>
+        <div id="0" className="cell"></div>
+        <div id="1" className="cell"></div>
+        <div id="2" className="cell"></div>
+        <div id="3" className="cell"></div>
+        <div id="4" className="cell"></div>
+        <div id="5" className="cell"></div>
+        <div id="6" className="cell"></div>
+        <div id="7" className="cell"></div>
+        <div id="8" className="cell"></div>
+      </div>
+      {gameOver ? (
+        <div className="result">
+          <p>{winner + " wins!"}</p>
+          <button className="btn" onClick={handlePlayAgain}>
+            Play again
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+      {tie ? (
+        <div className="result">
+          <p>{"Tie!"}</p>
+          <button className="btn" onClick={handlePlayAgain}>
+            Play again
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default TicTacToe;
---
+++ src/features/methods.js
@@ -0,0 +1,211 @@
+let methods = {
+  cellEmpty: (cellId) => {
+    const cell = document.getElementById(cellId);
+    //cell is empty
+    if (cell.innerHTML.trim() === "") {
+      return true;
+    }
+    // cell has value
+    return false;
+  },
+  placeMark: (cellId, playerIcon, setMarkCount) => {
+    const cell = document.getElementById(cellId);
+    cell.innerHTML = playerIcon;
+    setMarkCount((prev) => prev + 1);
+  },
+  checkGameOver: (
+    setGameOver,
+    setWinner,
+    crossMark,
+    circleMark,
+    setTie,
+    markCount,
+    winner,
+    setXScore,
+    setOScore
+  ) => {
+    const cell = document.getElementsByClassName("cell");
+    const slashmark = document.getElementById("slashmark");
+
+    //------------------------check row----------------------
+    //first row
+    if (
+      cell[0].innerHTML == cell[1].innerHTML &&
+      cell[1].innerHTML == cell[2].innerHTML &&
+      cell[0].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[0].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(90deg) translateX(-80px)";
+    }
+    //second row
+    else if (
+      cell[3].innerHTML == cell[4].innerHTML &&
+      cell[4].innerHTML == cell[5].innerHTML &&
+      cell[3].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[3].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(90deg) translateX(0px)";
+    }
+    //third row
+    else if (
+      cell[6].innerHTML == cell[7].innerHTML &&
+      cell[7].innerHTML == cell[8].innerHTML &&
+      cell[6].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[6].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(90deg) translateX(80px)";
+    }
+    //---------------------------------check column----------------
+    //first column
+    if (
+      cell[0].innerHTML == cell[3].innerHTML &&
+      cell[3].innerHTML == cell[6].innerHTML &&
+      cell[0].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[0].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(0deg) translateX(-80px)";
+    }
+    //second column
+    else if (
+      cell[1].innerHTML == cell[4].innerHTML &&
+      cell[4].innerHTML == cell[7].innerHTML &&
+      cell[1].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[1].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(0deg) translateX(0px)";
+    }
+    //third column
+    else if (
+      cell[2].innerHTML == cell[5].innerHTML &&
+      cell[5].innerHTML == cell[8].innerHTML &&
+      cell[2].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[2].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(0deg) translateX(80px)";
+    }
+    //----------------------check diagonal----------------
+    //first diagonal
+    if (
+      cell[0].innerHTML == cell[4].innerHTML &&
+      cell[4].innerHTML == cell[8].innerHTML &&
+      cell[0].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[0].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(-45deg) translateX(-3px)";
+    }
+    //second
+    else if (
+      cell[2].innerHTML == cell[4].innerHTML &&
+      cell[4].innerHTML == cell[6].innerHTML &&
+      cell[2].innerHTML.trim() !== ""
+    ) {
+      checkAndSetWinner(
+        cell[2].innerHTML,
+        setWinner,
+        crossMark,
+        circleMark,
+        setGameOver,
+        setXScore,
+        setOScore
+      );
+      slashmark.style.opacity = "1";
+      slashmark.style.transform = "rotate(45deg) translateX(2px)";
+    }
+    //---------------------------checkTie--------------
+    else if (markCount == 8 && winner != null) {
+      setTie(true);
+    }
+  },
+  resetCells: () => {
+    const slashmark = document.getElementById("slashmark");
+    slashmark.style.opacity = "0";
+    const cell = document.querySelectorAll(".cell");
+    cell.forEach((el) => {
+      el.innerHTML = null;
+    });
+  },
+};
+
+//extra functions for above methods
+export const checkAndSetWinner = (
+  stringContent,
+  setWinner,
+  crossMark,
+  circleMark,
+  setGameOver,
+  setXScore,
+  setOScore
+) => {
+  if (stringContent == crossMark) {
+    setXScore((prev) => prev + 1);
+    setWinner("X");
+  } else if (stringContent == circleMark) {
+    setOScore((prev) => prev + 1);
+    setWinner("O");
+  }
+  setGameOver(true);
+};
+export const { cellEmpty, placeMark, checkGameOver, resetCells } = methods;
